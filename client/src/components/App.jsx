d18ddf58199dd1ca108d5a007ef203b961bf51f2
Bruce <dieselrabago@gmail.com>
2022-08-09 09:39:55 -0700

    Framework buildout.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// import axios from 'axios';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <h1>
+          Generate Waifus!!!
+        </h1>
+      </div>);
+  }
+}
+
+export default App;
